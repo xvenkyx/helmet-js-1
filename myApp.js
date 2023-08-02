03c5bff0ab4b75c582299203849ab861094f0dc0
S Venkatesh <90242936+xvenkyx@users.noreply.github.com>
2023-08-02 21:41:40 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,9 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3030;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-})
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// })
 
 
 
