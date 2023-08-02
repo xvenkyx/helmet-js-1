11d1a4b8af93834f5c3dff9172850e08a64bf474
S Venkatesh <90242936+xvenkyx@users.noreply.github.com>
2023-08-02 22:59:47 +0530

    Added Helmet Frameguard

--- myApp.js
+++ myApp.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(helmet.hidePoweredBy());
 
+app.use(helmet.frameguard({
+  action:'deny'
+}))
+
 const PORT = process.env.PORT || 3030;
 
 // app.listen(PORT, () => {
