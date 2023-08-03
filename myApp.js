d25f77a3e9652ca8ec20b8a99db40a123c1ddcdb
S Venkatesh <90242936+xvenkyx@users.noreply.github.com>
2023-08-03 17:24:20 +0530

    Corrected CSP

--- myApp.js
+++ myApp.js
@@ -21,7 +21,7 @@ app.use(helmet.noCache());
 app.use(helmet.contentSecurityPolicy({
   directives: {
     defaultSrc : ["'self'"],
-    scriptSrc : ["'self'", "'trusted-cdn.com'"]
+    scriptSrc : ["'self'", 'trusted-cdn.com']
   }
 }))
 
