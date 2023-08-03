cc0c086ed20eaa633995c64e419105e5993feb4f
S Venkatesh <90242936+xvenkyx@users.noreply.github.com>
2023-08-03 15:12:14 +0530

    Added DNS Prefetch Control

--- myApp.js
+++ myApp.js
@@ -14,6 +14,8 @@ app.use(helmet.noSniff());
 
 app.use(helmet.ieNoOpen());
 
+app.use(helmet.useDnsPrefetchControl();
+
 ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({
   maxAge: ninetyDaysInSeconds, force: true
